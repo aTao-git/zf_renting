da104b601b463d0b37899487718fb62913b270c3
shentao <656036863@qq.com>
2019-08-31 20:13:44 +0800

    第一次提交

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'antd-mobile';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>react</h1>
+      <Button>按钮</Button>
+      </div>
+    )
+  }
+}
