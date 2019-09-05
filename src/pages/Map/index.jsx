b3c937694de8dd79d45e6f3bdd9fb046ca1a312b
aTao-git <52392030+aTao-git@users.noreply.github.com>
2019-09-05 13:54:52 +0800

    地图页面交互完成

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -7,6 +7,14 @@ const BMap = window.BMap
 export default class MapPages extends React.Component {
     // 全局的地图对象
     Map = null
+    // 设置不同级别地图应该显示的信息
+    Sites = [
+        { zoom: 11, level: 1, shape: 'circle', name: '区域' },
+        { zoom: 14, level: 2, shape: 'circle', name: '村子' },
+        { zoom: 16, level: 3, shape: 'rect', name: '街道' }
+    ]
+    // 设置索引，根据索引显示不同的级别
+    SitesIndex = 0
     // eslint-disable-next-line no-useless-constructor
     constructor() {
         super()
@@ -41,7 +49,7 @@ export default class MapPages extends React.Component {
         this.Map = new BMap.Map("mapmap")
         // 创建点坐标  
         // 初始化地图，设置中心点坐标和地图级别
-        this.Map.centerAndZoom(cityName, 15)
+        this.Map.centerAndZoom(cityName, this.Sites[this.SitesIndex].zoom)
         // 缩放组件
         this.Map.addControl(new BMap.NavigationControl())
         // 比例尺
@@ -49,25 +57,39 @@ export default class MapPages extends React.Component {
         const cityInfo = (await axios.get('/area/info?name=' + cityName)).body
         this.drawCityHouse(cityInfo)
     }
-    async drawCityHouse (cityInfo) {
+    async drawCityHouse(cityInfo) {
         const res = (await axios.get('/area/map?id=' + cityInfo.value)).body
         console.log(res)
+        this.Map.clearOverlays()
+        if (this.Sites[this.SitesIndex].zoom !== this.Sites[0].zoom) {
+            const point = new BMap.Point(cityInfo.coord.longitude, cityInfo.coord.latitude)
+            this.Map.centerAndZoom(point, this.Sites[this.SitesIndex].zoom)
+        }
         res.forEach(v => {
             const point = new BMap.Point(v.coord.longitude, v.coord.latitude)
-            this.Map.centerAndZoom(point, 15)
             const opts = {
-                position : point,    // 指定文本标注所在的地理位置
-                offset   : new BMap.Size(30, -30)    //设置文本偏移量
-              }
-              var label = new BMap.Label("欢迎使用百度地图，这是一个简单的文本标注哦~", opts);  // 创建文本标注对象
-                  label.setStyle({
-                       color : "red",
-                       fontSize : "12px",
-                       height : "20px",
-                       lineHeight : "20px",
-                       fontFamily:"微软雅黑"
-                   });
-              this.Map.addOverlay(label)
+                position: point,    // 指定文本标注所在的地理位置
+                offset: new BMap.Size(30, -30)    //设置文本偏移量
+            }
+            let label = ''
+            if (this.Sites[this.SitesIndex].shape === 'circle') {
+                label = new BMap.Label(`<div class=${styles.circle}>${v.label}<hr>${v.count}套</div>`, opts);  // 创建文本标注对象
+            } else if (this.Sites[this.SitesIndex].shape === 'rect') {
+                label = new BMap.Label(`<div class=${styles.rect} > ${v.label} ${v.count}套 </div>`, opts);
+            }
+            label.setStyle({
+                border: 'none',
+                backgroundColor: 'transparent'
+            });
+            label.addEventListener('click', () => {
+                if (this.SitesIndex === this.Sites.length) {
+                    this.Map.panTo(point)
+                } else {
+                    this.drawCityHouse(v)
+                }   
+            })
+            this.Map.addOverlay(label)
         })
+        this.SitesIndex++
     }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,20 @@
 import Axios from 'axios'
 import { REACT_APP_API_URL } from './urls'
+import { Toast } from 'antd-mobile'
 export let axios = Axios.create({
   baseURL: REACT_APP_API_URL
 })
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  Toast.loading('正在加载...', 60 * 60, null, true)
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
 axios.interceptors.response.use(function (res) {
+  Toast.hide()
   return res.data
 }, function (err) {
   console.log(err)
