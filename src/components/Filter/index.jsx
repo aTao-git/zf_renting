4cdb00951f5a8493784775fd899aa0329014e5c4
aTao-git <52392030+aTao-git@users.noreply.github.com>
2019-09-09 13:29:36 +0800

    筛选页面完成

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -4,10 +4,11 @@ import store from '../../store'
 import styles from './index.module.scss'
 import { PickerView } from 'antd-mobile'
 export default class Filter extends React.Component {
+    Unsubscribe = null
     // eslint-disable-next-line no-useless-constructor
     constructor() {
         super()
-        store.subscribe(this.getFilter)
+        this.Unsubscribe = store.subscribe(this.getFilter)
         this.state = {
             filterTitle: [
                 { name: "区域", level: 3 },
@@ -15,8 +16,11 @@ export default class Filter extends React.Component {
                 { name: "租金", level: 1 },
                 { name: "筛选", level: 0 }
             ],
-            selectIndex: 0,
-            filterList: []
+            selectIndex: -1,
+            filterList: [],
+            filterValues: [[], [], [], []],
+            pickerValue: [],
+            otherValue: []
         }
     }
     componentDidMount() {
@@ -25,7 +29,64 @@ export default class Filter extends React.Component {
             this.getFilter()
         }
     }
+    componentWillUnmount () {
+        this.Unsubscribe()
+    }
+    renderFilterContent = () => {
+        const { selectIndex, otherValue } = this.state
+        if (selectIndex === this.state.filterTitle.length - 1) {
+            return (
+                <div className={styles.filter_other}>
+                    {/* 遮罩层 */}
+                    <div className={styles.filter_other_mask} onClick={() => this.setState({ selectIndex: -1 })}></div>
+                    <div className={styles.filter_other_main}>
+                        <div className={styles.filter_other_main_list}>
+                            {this.state.filterList[selectIndex].map((v, i) =>
+                                <div key={i} className={styles.filter_other_group}>
+                                    <div className={styles.filter_other_group_title}>{v.name}</div>
+                                    <div className={styles.filter_other_group_content}>
+                                        {v.list.map((vv, ii) =>
+                                            <div
+                                                onClick={this.filterOtherItemClick.bind(this, vv.value)}
+                                                key={ii}
+                                                className={[styles.filter_other_item, otherValue.includes(vv.value) ? styles.filter_other_item_active : ''].join(' ')}
+                                            >{vv.label}</div>
+                                        )}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        <div className={styles.filter_other_main_btn}>
+                            <span onClick={this.filterOtherClear} >清除</span>
+                            <span onClick={this.handleFilterOk} >确定</span>
+                        </div>
+                    </div>
+                </div>
+            )
+        } else if (this.state.selectIndex <= 2 && this.state.selectIndex >= 0) {
+            return (
+                <div className={styles.filter_normal}>
+                    <PickerView
+                        data={this.state.filterList[this.state.selectIndex]}
+                        value={this.state.pickerValue}
+                        cols={this.getChildrenLevel(this.state.filterList[this.state.selectIndex])}
+                        onChange={this.pickChange}
+                    />
+                    <div className={styles.filter_normal_btn}>
+                        <span onClick={() => this.setState({ selectIndex: -1 })}>取消</span>
+                        <span onClick={this.handleFilterOk} >确定</span>
+                    </div>
+                </div>
+            )
+        }
+        return (
+            <></>
+        )
+    }
     render() {
+        if (!this.state.filterList.length) {
+            return <></>
+        }
         return (
             <React.Fragment>
                 <div className={styles.zf_filter}>
@@ -38,11 +99,8 @@ export default class Filter extends React.Component {
                             )
                         })}
                     </div>
-                    <PickerView
-          data={this.state.filterList[this.state.selectIndex]}
-          value={this.state.filterList[this.state.selectIndex]}
-          cols={this.state.filterTitle[this.state.selectIndex].level}
-        />
+                    {this.renderFilterContent()}
+
                 </div>
             </React.Fragment>
         )
@@ -53,12 +111,12 @@ export default class Filter extends React.Component {
         cityName = '广州'
         const id = (await axios.get("/area/info?name=" + cityName)).body.value
         const res = await axios.get("/houses/condition?id=" + id)
-        const { area, subway, rentType, price } = res.body
+        const { area, subway, rentType, price, roomType, oriented, floor, characteristic } = res.body
         let filterList = [
             [area, subway],
             rentType,
             price,
-            []
+            [{ name: "户型", list: roomType }, { name: "朝向", list: oriented }, { name: "楼层", list: floor }, { name: "亮点", list: characteristic }]
         ]
         this.setState({
             filterList
@@ -69,4 +127,60 @@ export default class Filter extends React.Component {
             selectIndex: i
         })
     }
+    getChildrenLevel(arr) {
+        let set = new Set()
+        let index = 0
+        const getChildren = (arr) => {
+            if (arr.length) {
+                arr.forEach(v => {
+                    if (v.children) {
+                        index++
+                        getChildren(v.children)
+                        index--
+                    } else {
+                        set.add(index)
+                    }
+                })
+            } else {
+                set.add(index)
+            }
+        }
+        getChildren(arr)
+        return Math.max.apply(null, [...set]) || 1
+    }
+    pickChange = (value) => {
+        const { selectIndex, filterValues } = this.state
+        filterValues[selectIndex] = value
+        this.setState({
+            filterValues,
+            pickerValue: value
+        })
+    }
+    filterOtherItemClick = (value) => {
+        let { otherValue, filterValues, selectIndex } = this.state
+        const index = otherValue.findIndex(v => v === value)
+        if (index === -1) {
+            otherValue.push(value)
+        } else {
+            otherValue.splice(index, 1)
+        }
+        filterValues[selectIndex] = otherValue
+        this.setState({ otherValue, filterValues })
+    }
+    handleFilterOk = () => {
+        const { filterValues } = this.state
+        this.props.getFilterValues(filterValues)
+        this.setState({
+            selectIndex: -1
+        })
+    }
+    filterOtherClear = () => {
+        let { filterValues, selectIndex } = this.state
+        const otherValue = []
+        filterValues[selectIndex] = otherValue
+        this.setState({
+          otherValue,
+          filterValues
+        })
+      }
 }
--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -4,7 +4,36 @@ import SearchInput from '../Home/SearchInput'
 import styles from "./index.module.scss"
 import { withRouter } from "react-router-dom"
 import Filter from "../../components/Filter"
-class List extends React.Component {
+import store from '../../store'
+import { axios } from '../../utils/request'
+import { List, AutoSizer } from "react-virtualized"
+import { REACT_APP_API_URL } from '../../utils/urls'
+class HouseList extends React.Component {
+  Unsubscribe = null
+  QueryParams = {
+    start: 1,
+    end: 10
+  }
+  pageSize = 10
+  count = 10
+  isLoadding = false
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super()
+    this.state = {
+      hkList: []
+    }
+    this.Unsubscribe = store.subscribe(this.getList)
+  }
+  componentDidMount() {
+    const cityName = store.getState().mapReducer.cityName
+    if (cityName) {
+      this.getList()
+    }
+  }
+  componentWillUnmount() {
+    this.Unsubscribe()
+  }
   render() {
     return (
       <React.Fragment>
@@ -14,7 +43,7 @@ class List extends React.Component {
             icon={<Icon type="left" />}
             onLeftClick={() => this.props.history.go(-1)}
             style={{
-              backgroundColor:"#cccccc"
+              backgroundColor: "#cccccc"
             }}
           >
             <div className={styles.hk_list_header}>
@@ -23,14 +52,102 @@ class List extends React.Component {
           </NavBar>
           {/* 过滤 开始 */}
           <div className="list_fitler">
-          <Filter />
+            <Filter getFilterValues={this.getFilterValues} />
           </div>
           {/* 过滤 结束 */}
-
-
+          {/* 列表 开始 */}
+          <div className={styles.hk_list_content}>
+            <AutoSizer>
+              {({ height, width }) => (
+                <List
+                  height={height}
+                  width={width}
+                  rowCount={this.state.hkList.length}
+                  rowHeight={160}
+                  rowRenderer={this.rowRenderer}
+                  onScroll={this.handleScroll}
+                />
+              )}
+            </AutoSizer>
+          </div>
+          {/* 列表 结束 */}
         </div>
       </React.Fragment>
-    );
+    )
+  }
+  getFilterValues = async (value) => {
+    const params = {
+      [value[0][0]]: value[0][2] !== "null" ? value[0][2] : value[0][1],
+      rentType: value[1][0],
+      price: value[2][0],
+      more: value[3].join(',')
+    };
+
+    // 拼接成完整的参数
+    this.QueryParams = Object.assign(this.QueryParams, params);
+    for (const key in this.QueryParams) {
+      if (this.QueryParams[key] === undefined || this.QueryParams[key] === "") {
+        delete this.QueryParams[key]
+      }
+    }
+
+    this.setState({ hkList: [] })
+    console.log(this.QueryParams)
+    this.getList()
+  }
+  getList = async () => {
+    this.isLoadding = true;
+    const cityName = store.getState().mapReducer.cityName
+
+    if (!this.QueryParams.cityId) {
+      const id = (await axios.get("/area/info?name=" + cityName)).body.value
+      this.QueryParams.cityId = id
+    }
+
+    const res = await axios.get("/houses", { params: this.QueryParams })
+    console.log(res)
+    this.count = res.body.count
+    const { hkList } = this.state
+    this.setState({ hkList: [...hkList, ...res.body.list] })
+    // 表示 数据已经请求回来了 isLoadding false
+    this.isLoadding = false
+  }
+  rowRenderer = ({ key, index, style }) => {
+    const { hkList } = this.state;
+    return (
+      <div
+        key={key}
+        style={style}
+      >
+        <div onClick={() => this.props.history.push("/Detail/" + hkList[index].houseCode)} className={styles.house_item}>
+          <div className={styles.house_item_img_wrap}>
+            <img src={REACT_APP_API_URL + hkList[index].houseImg} alt="" />
+          </div>
+          <div className={styles.house_item_info_wrap}>
+            <div className={styles.house_info1}>{hkList[index].title}</div>
+            <div className={styles.house_info2}>{hkList[index].desc}</div>
+            <div className={styles.house_info3}>{hkList[index].tags.map(vv => <span key={vv} > {vv}</span>)}</div>
+            <div className={styles.house_info4}><span>{hkList[index].price}</span>元/月  </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  handleScroll = ({ clientHeight, scrollHeight, scrollTop }) => {
+    if (scrollHeight - clientHeight - scrollTop <= 20) {
+      // 判断 还有没有下一页的数据
+      if (this.QueryParams.end < this.count) {
+        if (!this.isLoadding) {
+          // 还有数据
+          this.QueryParams.start += this.pageSize
+          this.QueryParams.end += this.pageSize
+          this.getList()
+        }
+      } else {
+        console.log("没有数据了")
+      }
+    }
+
   }
 }
-export default withRouter(List)
+export default withRouter(HouseList)
